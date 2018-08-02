caf0ae841e8327042d9d5a051148f81f01005538
taeha <taeha>
2018-08-03 08:07:58 +0900

    refactor slice 메서드는 원래 array에만 정의되어있음 하지만 array에만 활용하는 것이 아닌, 다양한 arraylike 객체에 활용하기 위한 slice 활용법

--- reduce.js
+++ reduce.js
@@ -1,6 +1,7 @@
 //기본적으로 원래자료와는 다르게 축약된 형태로 사용하기 위해 주로 사용됨
 //ex. array로 숫자 하나를 뽑아내거나 or array로 객체 하나를 만들어 내거나
 
+
 function _each(list,iter){
     for(var i=0;i<list.length;i++){
         iter(list[i]);
@@ -8,7 +9,32 @@ function _each(list,iter){
     return list;
 }
 
+//앞서 작성한 each, map, filter 는 인자가 array가 아니어도 사용 가능했던것처럼 
+//reduce도 똑같이
 function _reduce(list, iter, memo) {
+    if (arguments.length == 2) {
+        memo = list[0];
+
+        //slice 메서드는 array에서만 사용이 가능함
+        //slice 메서드는 앞에서 부터 인자로 주는 갯수만큼 배열을 잘라서 리턴하는 메서드
+        list = list.slice(1);
+
+        //따라서 함수 사용의 범용성을 위해 다음과 같이 활용할수 있는데
+        var slice = Array.prototype.slice;
+        slice.call(a,2)
+        slice.call(a,2).constructor;
+
+        //다음과 같은 array처럼 동작할 수 있는 객체에서
+        var a = { 0: 1, 1:20,2:30, length:3 };
+        a[0]
+        //1
+        a[1]
+        //20
+        a[2]
+        //30
+        slice.call(a,1);
+        //[20,30]
+    }
     _each(list,function(val){
         memo = iter(memo,val);
     });
@@ -24,10 +50,23 @@ console.log (
         [1,2,3], function(a, b){
 
         },0));
-
+/* 즉 reduce는 요런 느낌
 memo = add(0,1);
 memo = add(memo,2);
 memo = add(memo,3);
 return memo;
-
+            */
 add(add(add(0,1),2),3);
+
+//세번째 인자를 생략하는 방법
+//ex
+console.log(
+    _reduce([1,2,3], add, 10)
+);
+//16
+console.log(
+    _reduce([1,2,3], add)
+);
+ //6
+//==> add(add(1,2),3);
+
