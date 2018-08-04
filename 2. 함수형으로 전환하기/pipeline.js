23597f401f49b9376e5a9730ca85f224e2195c13
taeha <91haba@gmail.com>
2018-08-05 00:18:09 +0900

    _go 함수 정의 및 활용

--- 2. 함수형으로 전환하기/pipeline.js
+++ 2. 함수형으로 전환하기/pipeline.js
@@ -83,3 +83,83 @@
         function(a) {return a * a;},
         console.log()
     )
+
+    //3.users에 go 적용
+
+    var users = [
+
+        {id: 1, name: 'ID', age:36 },
+        {id: 2, name: 'BJ', age:32 },
+        {id: 3, name: 'JM', age:32 },
+        {id: 4, name: 'PJ', age:27 },
+        {id: 5, name: 'HA', age:25 },
+        {id: 6, name: 'JE', age:26 },
+        {id: 7, name: 'JI', age:31 },
+        {id: 8, name: 'MP', age:23 }
+    
+    ]
+
+    function _map(list,mapper){
+        var new_list = [];
+        for(var i=0;i<list.length;i++){
+            new_list.push(mapper(list[i]));
+        }
+        return new_list;
+    }
+    function _filter(users,predi){
+        var new_list = [];
+        for(var i=0;i<users.length;i++){
+            if(predi(users[i])){
+                new_list.push(users[i]);
+            }
+        }
+        return new_list;
+    }
+    //curry
+    function _curry(fn){
+        return function(a){
+            //인자가 2개 들어오면 return하지 않고 즉시실행 하는 코드 삽입
+            //인자가 1개씩 들어오면 함수 실행을 미룸
+            /*if (arguments.length == 2) return fn(a, b);
+            return function(b){
+                return fn(a,b);
+            }*/
+            //refactor
+            return arguments.length == 2 ? fn(a,b) : function(b){ return fn(a,b); };
+        }
+    }
+    //curryr
+    function _curryr(fn){
+        return function(a,b){
+            return arguments.length == 2 ? fn(a,b) : function(b){ return fn(b, a)};
+        }
+    }
+    var _get = _curryr(function(obj,key){
+        return obj == null ? undefined : obj[key];
+    });
+    /*
+    console.log(
+        _map(
+            _filter(users,function(user){return user.age>=30;}),
+        _get('name'))
+    );
+    console.log(
+        _map(
+            _filter(users,function(user){return user.age<30;}),
+        _get('age'))
+    );*/
+
+    //위와 같은 함수를 
+    _go(users, //users가 들어오면
+        function(users) {
+            //users를 filter 한 것을
+            return _filter(users,function(user){
+                return user.age >= 30;
+            });
+        },
+        function(users) { //filter된 users를 받아옴
+            return _map(users,_get('name'));
+        },
+        //console.log까지 담아줌
+        console.log
+    );
